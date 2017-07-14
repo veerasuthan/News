bc84cf179cc1e354034d6ca59abda7357f4d9eba
veerasuthan <veerasuthan.v@knowledgeq.com>
2017-07-14 20:32:02 +0530

    added base setup

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor() {
+      super();
+  }
+  componentDidMount() {
+  }
+  render() {
+      return (
+          <div>Helloworld</div>
+      );
+  }
+}
